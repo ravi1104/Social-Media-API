bb3ecbbff0db2ce9732b412b1c322f683c7d25e7
Ravi Ranjan Yadav <66769928+ravi1104@users.noreply.github.com>
2024-09-08 10:23:58 +0530

    Update server.js

--- server.js
+++ server.js
@@ -3,5 +3,5 @@ import { connectToDb } from "./src/config/db.js";
 const PORT = process.env.PORT || 3000;
 server.listen(PORT, async () => {
   await connectToDb();
-  console.log(`server is running at port 3000`);
+  console.log(`server is running at port ${PORT}`);
 });
